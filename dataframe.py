0a28416f89ad55e451bdd98522eb9dbf6e62571d
CKVCU <cknight5@vcu.edu>
2026-09-18 00:23:14 +0000

    plotting

---
+++ dataframe.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import sqlite3 
+
+conn=sqlite3.connect('project.db')
+#create a ataframe from the database
+df_weather=pd.read_sql_query("SELECT * FROM WEATHER", conn)
+print(df_weather.head())
+print(df_weather.describe())
+df_weather.plot(x='time', y='temperature_C', kind='line')
+plt.savefig('temperature_plot.png')
